6b620868e2b1af40d409903970a074d185b472ad
AlexxNB <iam@alexxnb.ru>
2021-03-14 20:05:31 +0300

    fix: :bug: Do not show commit in list  if it uses as version tag

--- src/lib/git.js
+++ src/lib/git.js
@@ -32,9 +32,12 @@ export function getHistory(conventional){
 
     tree.addVersion('unreleased',null);
 
-    commits.forEach( commit => {
+    commits.forEach( (commit,i) => {
         const match = commit.subject.match(/^(?:v|v\.|v\.\s+)?(\d\.\d\.\d.*)/);
-        if(match) tree.addVersion(match[1],commit.date);
+        if(match) {
+            tree.addVersion(match[1],commit.date);
+            delete commits[i];
+        }
     });
 
     tags.forEach( tag => {
