7eaa3cd63878db6deabaa1a291de426a63cd8cf1
AlexxNB <iam@alexxnb.ru>
2021-03-15 14:48:49 +0300

    feat: :sparkles: Add action and workflow for it

--- src/action.js
+++ src/action.js
@@ -1,1 +1,42 @@
-console.log('hello')
+import core from '@actions/core';
+import github from'@actions/github';
+import {render} from 'chalogen';
+import fs from 'fs';
+
+try {
+  const opts = {
+      title: core.getInput('title'),
+      list: core.getInput('list'),
+      date: core.getInput('date'),
+      file: core.getInput('file'),
+      version: core.getInput('version'),
+      unreleased: core.getInput('unreleased'),
+      hideUnreleased: core.getInput('hide-unreleased'),
+      hideTitle: core.getInput('hide-title'),
+      hideBody: core.getInput('hide-body'),
+      output: 'cli'
+  }
+
+  console.log('Rendering markdown...');
+  render(makeOptions(opts));
+  opts.output = 'markdown';
+  const body = render(makeOptions(opts));
+  core.setOutput('changelog', body);
+  console.log('Saving to '+opts.file);
+  fs.writeFileSync(opts.file,body);
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+function makeOptions(opts){
+    return {
+        title: opts.title,
+        output: opts.output || 'cli',
+        showTypes: opts.list.split(','),
+        dateFormat: opts.date,
+        showUnreleased: opts.hideUnreleased !== false,
+        showBody: opts.hideTitle !== false,
+        showTitle: opts.hideBody !== false,
+        onlyVersion: (!!opts.version && opts.version) || (opts.unreleased && 'unreleased'),
+    }
+}
