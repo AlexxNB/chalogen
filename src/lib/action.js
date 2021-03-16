2891c620b4b29faa28cf40d481cec94552ddff07
AlexxNB <iam@alexxnb.ru>
2021-03-16 10:48:04 +0300

    fix: :bug: Fix workflow file syntax

--- src/lib/action.js
+++ src/lib/action.js
@@ -4,7 +4,7 @@ import path from 'path';
 const DIR = path.join('.github','workflows');
 
 export function addAction(filename){
-    filename = (filename || 'generate_changelog1')+'.yml';
+    filename = (filename || 'generate_changelog')+'.yml';
     let filepath = path.join(DIR,filename);
 
     if(fs.existsSync(filepath)) {
@@ -20,25 +20,25 @@ function getActionBody(){
     return `name: Generate Changelog
 
 on:
-push:
+  push:
     branches: [ main, master]
 
 jobs:
-changelog:
+  changelog:
     name: Update Changelog
     runs-on: ubuntu-latest
     steps:
-    - name: Checkout code
+      - name: Checkout code
         uses: actions/checkout@v2
         with:
-        fetch-depth: 0
-    - name: Update Changelog
+          fetch-depth: 0
+      - name: Update Changelog
         uses: AlexxNB/chalogen@master
         with:
-        title: My project changelog 
-    - name: Commit Changelog to repository
+          title: My project changelog 
+      - name: Commit Changelog to repository
         uses: stefanzweifel/git-auto-commit-action@v4
         with:
-        commit_message: 'docs(Changelog): Update Changelog'
-        file_pattern: CHANGELOG.md`
+          commit_message: 'docs(Changelog): Update Changelog'
+          file_pattern: CHANGELOG.md`
 }
